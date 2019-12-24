2bb4252357e6a418b700f92c0082463df1aa0a2f
tusphananh <59197844+tusphananh@users.noreply.github.com>
2019-12-24 23:47:35 +0700

    Add files via upload

---
+++ src/UseInfo/User.java
@@ -0,0 +1,16 @@
+package UserInfo;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
